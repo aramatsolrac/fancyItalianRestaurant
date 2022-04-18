1498d6126f8a592fadf3d83f4e0fd8223f4c5307
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-18 17:57:41 -0600

    Add populateMenu helper function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 import menu from "./menu";
-import formatNumber from "./utils/formatNumber";
+import { populateMenu } from "./utils/helper";
 
 const starters = document.getElementById("starters");
 const pasta = document.getElementById("pasta");
@@ -9,37 +9,35 @@ const menuItems = menu.items.sort((a, b) => a.menuOrder - b.menuOrder);
 console.log(menuItems);
 
 menuItems.forEach((item) => {
-  if (item.type === "starters") {
-    const starterDiv = document.createElement("div");
-    starterDiv.classList.add("menu-item");
-    starterDiv.innerHTML = `
-      <h3>${item.name}</h3>
-      <p>${item.description}</p>
-      <p>${formatNumber(item.price)}</p>
-      <p>${item.menuOrder}</p>
-    `;
-    starters.appendChild(starterDiv);
-  } else if (item.type === "pasta") {
-    const pastaDiv = document.createElement("div");
-    pastaDiv.classList.add("menu-item");
-    pastaDiv.innerHTML = `
-      <h3>${item.name}</h3>
-      <p>${item.description}</p>
-      <p>${formatNumber(item.price)}</p>
-      <p>${item.menuOrder}</p>
-
-    `;
-    pasta.appendChild(pastaDiv);
-  } else if (item.type === "pizza") {
-    const pizzaDiv = document.createElement("div");
-    pizzaDiv.classList.add("menu-item");
-    pizzaDiv.innerHTML = `
-      <h3>${item.name}</h3>
-      <p>${item.description}</p>
-      <p>${formatNumber(item.price)}</p>
-      <p>${item.menuOrder}</p>
-
-    `;
-    pizza.appendChild(pizzaDiv);
-  }
+  console.log(item.spicy);
+  populateMenu(
+    "starters",
+    starters,
+    item.name,
+    item.description,
+    item.price,
+    item.menuOrder,
+    item.type,
+    item.spicy
+  );
+  populateMenu(
+    "pasta",
+    pasta,
+    item.name,
+    item.description,
+    item.price,
+    item.menuOrder,
+    item.type,
+    item.spicy
+  );
+  populateMenu(
+    "pizza",
+    pizza,
+    item.name,
+    item.description,
+    item.price,
+    item.menuOrder,
+    item.type,
+    item.spicy
+  );
 });
